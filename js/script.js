d9064ab7dafc61126902d1ae52fee9bd0a22f23e
Yulia Akutina <akutinayulia@gmail.com>
2018-04-01 13:30:37 +0300

    Оживила модальное окно с помощью js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
 var gllacyMap;
 var gllacyPlacemark;
 
+var link = document.querySelector(".contacts__btn");
+var modal = document.querySelector(".modal-feedback");
+var close = modal.querySelector(".modal-feedback__btn-close");
+var username = modal.querySelector("[name=user-name]");
+var form = modal.querySelector("form");
+var email = modal.querySelector("[name=user-email]");
+var message = modal.querySelector("[name=beedback]");
+var overlay =document.querySelector(".overlay");
+
 ymaps.ready(function () {
   gllacyMap = new ymaps.Map("map", {
     center: [59.939346, 30.329182],
@@ -15,4 +24,38 @@ ymaps.ready(function () {
     iconImageOffset: [-40, -139]
   });
   gllacyMap.geoObjects.add(gllacyPlacemark);
+});
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal-feedback--open");
+  overlay.classList.add("overlay--open");
+  username.focus();
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.remove("modal-feedback--open");
+  overlay.classList.remove("overlay--open");
+  modal.classList.remove("modal-feedback--error");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!username.value || !email.value || !message.value) {
+  evt.preventDefault();
+  modal.classList.remove("modal-feedback--error");
+  modal.offsetWidth = modal.offsetWidth;
+  modal.classList.add("modal-feedback--error");
+}
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (modal.classList.contains("modal-feedback--open")) {
+      modal.classList.remove("modal-feedback--open");
+      overlay.classList.remove("overlay--open");
+      modal.classList.remove("modal-feedback--error");
+    }
+  }
 });
